b6abaa2449fd59e2a43c190cd4be6c9ea9bb0612
Matt Wyen <matt@mattwyen.me>
2019-10-08 15:26:26 -0400

    update

--- development/rescan_dev_test.rb
+++ development/rescan_dev_test.rb
@@ -1,18 +1,22 @@
-require 'nexpose'
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
 include Nexpose
+
 # Create a new InsightVM Connection the the default port
 nsc = Connection.new('localhost', 'USERNAME-HERE', 'PASSWORD-HERE')
 nsc.login # Login to NSC and Establish a session id
 # Beginning of rescan assets
-asset_group_ids = [1,4,21]
+asset_group_ids = [1, 4, 21]
 asset_group_ids.each do |id|
   group = AssetGroup.load(nsc, id)
-    # 30.downto(0) do |i|
-    # =>  puts "00:00:#{'%02d' % i}"
-    # =>  sleep 1
-    # end
-  sleep 60 #sleep 60 seconds to prevent the console from overloading or not finishing the task
+  # 30.downto(0) do |i|
+  # =>  puts "00:00:#{'%02d' % i}"
+  # =>  sleep 1
+  # end
+  sleep 60 # sleep 60 seconds to prevent the console from overloading or not finishing the task
   group.rescan_assets(nsc)
-  puts "Started Scan..."
+  puts 'Started Scan...'
 end
 nsc.logout # log out of session
